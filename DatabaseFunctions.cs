432ff46823604f5f68eb0463b4ac3aded68eb1d4
natalimj <natalimj88@gmail.com>
2022-05-11 23:12:40 +0200

    add new database functions

--- DatabaseFunctions.cs
+++ DatabaseFunctions.cs
@@ -1,19 +1,25 @@
 using System;
-using System.IO;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json;
+using System.Collections.Generic;
+using SEP6_AzureFunctions.Models;
+using Microsoft.Azure.Cosmos;
 
 namespace SEP6_AzureFunctions
 {
     public static class DatabaseFunctions
     {
-        [FunctionName("adduserrating")]
+        // static CosmosClient cosmosClient = new CosmosClient("DatabaseConnectionString", new CosmosClientOptions() { AllowBulkExecution = true });
+        //static Microsoft.Azure.Cosmos.Database database = cosmosClient.GetDatabase("MovieAppDB");
+        //static Container container = database.GetContainer("UserRating");
 
+        static CosmosClient cosmosClient = new CosmosClient("DatabaseConnectionString");
+
+        [FunctionName("adduserrating")]
         public static async Task<IActionResult> AddUserRating(
         [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
         [CosmosDB(
@@ -38,10 +44,10 @@ namespace SEP6_AzureFunctions
                 {
                     id = System.Guid.NewGuid().ToString(),
                     timestamp = DateTime.Now.ToString("yyyy’-‘MM’-‘dd’T’HH’:’mm’:’ss.fffffffK"),
-                    username = "stephen",
+                    username = "leila",
                     movieid = "143936",
                     rating = 6
-                }); 
+                });
             }
 
             string responseMessage = string.IsNullOrEmpty(name)
@@ -50,5 +56,72 @@ namespace SEP6_AzureFunctions
 
             return new OkObjectResult(responseMessage);
         }
+
+
+
+
+        //http://localhost:7071/api/UserRating/timestamp/1
+        [FunctionName("getuserrating")]
+        public static IActionResult GetUserRating(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = "{CollectionName}/{timestamp}/{id}")] HttpRequest req,
+        [CosmosDB(
+        databaseName: "MovieAppDB",
+        collectionName: "UserRating",
+        ConnectionStringSetting = "DatabaseConnectionString",
+        Id ="{id}",
+        PartitionKey ="{timestamp}")] UserRating item,
+        ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            if (item == null)
+            {
+                return new NotFoundResult();
+            }
+
+            return new ObjectResult(item);
+
+        }
+
+
+        [FunctionName("getallratings")]
+        public static IActionResult GetAllRatings(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = null)] HttpRequest req,
+        [CosmosDB(
+        databaseName: "MovieAppDB",
+        collectionName: "UserRating",
+        ConnectionStringSetting = "DatabaseConnectionString",
+        SqlQuery = "SELECT * FROM c")] IEnumerable<object> documents,
+        ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+            return new OkObjectResult(documents);
+        }
+
+    
+
+
+        [FunctionName("deleterating")]
+        public async static Task<IActionResult> DeleteRating(
+        [HttpTrigger(AuthorizationLevel.Anonymous, "get", Route = "{timestamp}/{id}")] HttpRequest req,
+        [CosmosDB(
+        databaseName: "MovieAppDB",
+        collectionName: "UserRating",
+        ConnectionStringSetting = "DatabaseConnectionString",
+        Id ="{id}",
+        PartitionKey ="{timestamp}")] UserRating item,
+        ILogger log)
+
+        {     
+            Container container = cosmosClient.GetContainer("MovieAppDB","UserRating");
+ 
+            await container.DeleteItemAsync<UserRating>("1",new PartitionKey("timestamp"));
+            log.LogInformation("C# HTTP trigger function processed a request.An item has been deleted: ");
+            return new OkObjectResult("An item has been deleted: ");
+
+
+        }
+      
+   
+        }
     }
-}
---
+++ Models/UserRating.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SEP6_AzureFunctions.Models
+{
+    public class UserRating
+    {
+        [JsonProperty(PropertyName = "id")]
+        public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "timestamp")]
+        public string Timestamp { get; set; }
+
+        [JsonProperty(PropertyName = "username")]
+        public string Username { get; set; }
+
+        [JsonProperty(PropertyName = "movieid")]
+        public string MovieId { get; set; }
+
+        [JsonProperty(PropertyName = "rating")]
+        public double Rating { get; set; }
+
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
