fe2b9e9f242f7c7bac010fd2669d8f08c5c5086d
Bogdan Mezei <bogdan335@hotmail.com>
2022-05-16 23:11:23 +0200

    TC-12 UnitTesting

---
+++ AzureFunctionsUnitTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using Xunit;
+
+namespace AzureFunctionsUnitTest
+{
+
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
